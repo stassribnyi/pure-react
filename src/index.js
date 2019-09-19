87fe0797313ae382c9b740c532f96c74975706e7
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-09-19 18:24:00 +0100

    Create People app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-const App = () => {
+const Person = props => {
   return React.createElement(
-    'div',
-    {
-      className: 'App'
-    },
-    React.createElement('h1', null, 'Hello World!')
+    'h2',
+    null,
+    `${props.name.first} ${props.name.last}`
   );
 };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=10')
+      .then(response => response.json())
+      .then(data => data.results)
+      .then(people => this.setState({ people }));
+  }
+
+  render() {
+    return React.createElement(
+      'main',
+      {
+        className: 'App'
+      },
+      [
+        React.createElement('h1', { key: 0 }, 'Hello World!'),
+        this.state.people.map((person, i) =>
+          React.createElement(Person, { key: i + 1, ...person })
+        )
+      ]
+    );
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
