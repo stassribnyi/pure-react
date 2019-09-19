4483dda25e58d67d649cb5c925db56ff5d660060
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-09-19 20:01:09 +0100

    Add styling for person component and for app itself

--- src/components/person/person.component.js
+++ src/components/person/person.component.js
@@ -1,19 +1,31 @@
+const PersonName = props => {
+  return React.createElement('h4', { className: 'person__name' }, [
+    React.createElement(
+      'span',
+      { key: props.first, className: 'person__first-name' },
+      props.first
+    ),
+    ' ',
+    React.createElement(
+      'span',
+      { key: props.last, className: 'person__last-name' },
+      props.last
+    )
+  ]);
+};
+
 export const Person = props => {
-  return React.createElement(
-    'div',
-    { className: 'person' },
-    React.createElement('h2', { className: 'person__name' }, [
-      React.createElement(
-        'span',
-        { key: props.name.first, className: 'person__first-name' },
-        props.name.first
-      ),
-      ' ',
-      React.createElement(
-        'span',
-        { key: props.name.last, className: 'person__last-name' },
-        props.name.last
-      )
-    ])
-  );
+  return React.createElement('div', { className: 'person' }, [
+    React.createElement('img', {
+      key: props.picture.thumbnail,
+      src: props.picture.thumbnail,
+      className: 'person__img'
+    }),
+    React.createElement(PersonName, { key: props.id.value, ...props.name }),
+    React.createElement(
+      'h4',
+      { key: props.id.value + 1, className: 'person__email' },
+      `Email: ${props.email}`
+    )
+  ]);
 };
